b7628c5532de7cc3b3e44f20514d30ea565fe329
Anastasiia <blacsharm@gmail.com>
2024-06-07 17:23:51 +0300

    push

--- src/mongodb/initMongoConnection.js
+++ src/mongodb/initMongoConnection.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 export const initMongoConnection = async() => {
     try {
         const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
-        await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster`);
+        await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`);
         console.log('Mongo connection successfully established!');
 
     } catch (error) {
         console.log('Error while setting up mongo connection', error);
         process.exit(1);
     }
-};
+}; 
