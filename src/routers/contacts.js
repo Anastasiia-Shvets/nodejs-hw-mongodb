531da508ed8d277b4a4c584864b8fc93b2a44246
Anastasiia <blacsharm@gmail.com>
2024-06-13 14:42:42 +0300

    start

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { createContactController, getContactsController } from '../controllers/contacts.js';
 import { deleteContactController, getContactByIdController, patchContactController } from '../controllers/contactById.js';
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { validateBody } from '../utils/validateBody.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 
