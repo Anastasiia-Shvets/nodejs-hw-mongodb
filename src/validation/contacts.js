ab426e6aea0a17324375a23dddf51466f72e4fc6
Anastasiia <blacsharm@gmail.com>
2024-07-04 08:26:02 +0300

    correction

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,16 +2,16 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().required(),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean().optional(),
-    contactType: Joi.string().valid('work', 'home', 'personal').default('personal').required(),
+    contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
 });
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).optional(),
-    phoneNumber: Joi.string().min(3).max(20).optional(),
-    email: Joi.string().email().optional(),
-    isFavourite: Joi.boolean().optional(),
-    contactType: Joi.string().valid('work', 'home', 'personal').optional(),
-}).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+    isFavourite: Joi.boolean().default(false),
+    contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
+});
 
