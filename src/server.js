add5f488ca82daddd48686cc8863c3b67c6f4c98
Anastasiia <blacsharm@gmail.com>
2024-07-17 17:10:31 +0300

    corrected

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ import { UPLOAD_DIR } from './contacts/index.js';
 export const setupServer = () => {
     const app = express();
 
-    app.use(express.json());
+    app.use(
+        express.json({
+            type: ['application/json', 'application/vnd.api+json'],
+            limit: '100kb',
+        }),
+    );
     app.use(cors());
     app.use(cookieParser());
     app.use('/uploads', express.static(UPLOAD_DIR));
